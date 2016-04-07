3aec2d27cb5a541d936e226347a0828717c2f6dd
Vova Abdrakhmanov <369565@gmail.com>
2016-04-07 21:08:41 +0300

    [add] SL2: biaxial SL

--- src/material.rs
+++ src/material.rs
@@ -181,3 +181,141 @@ impl Material for SL {
         self.acoustic_constant * self.probability(self.energy(p))
     }
 }
+pub struct SL2 {
+    brillouin_zone: BrillouinZone,
+    minimum_energy: f64,
+    maximum_energy: f64,
+
+    // phonons:
+    optical_energy: f64,
+    acoustic_constant: f64,
+    optical_constant: f64,
+    energies: Vec<f64>,
+    probabilities: Vec<f64>,
+}
+
+impl SL2 {
+    pub fn new(optical_energy: f64, optical_constant: f64, acoustic_constant: f64) -> SL {
+        let a = Point::new(-PI, 0.0);
+        let b = Point::new(0.0, -PI);
+        let d = Point::new(0.0, PI);
+        let brillouin_zone = BrillouinZone::new(a, b, d);
+        let energy_samples = 1000usize;
+
+        let mut s = SL {
+            minimum_energy: 0.0,
+            maximum_energy: EPS1,
+            brillouin_zone: brillouin_zone,
+            optical_energy: optical_energy,
+            optical_constant: optical_constant,
+            acoustic_constant: acoustic_constant,
+            energies: Vec::new(),
+            probabilities: Vec::new(),
+        };
+
+
+        let mut energies = Vec::with_capacity(energy_samples);
+        let mut probabilities = vec![0f64; energy_samples];
+
+        let energy_step = (s.maximum_energy - s.minimum_energy) / (energy_samples as f64 - 1.0);
+        for i in 0..energy_samples {
+            let e = s.minimum_energy + energy_step * (i as f64);
+            energies.push(e);
+        }
+
+        let mut pool = Pool::new(4);
+
+        pool.scoped(|scope| {
+            for (index, item) in probabilities.iter_mut().enumerate() {
+                let ref material = s;
+                let error = 1e-5;
+                let energy = energies[index];
+                scope.execute(move || {
+                    *item = probability(energy, material, error);
+                });
+            }
+        });
+
+        s.energies = energies;
+        s.probabilities = probabilities;
+        s
+    }
+
+
+    fn probability(&self, energy: f64) -> f64 {
+        let step = self.energies[1] - self.energies[0];
+
+        if energy < self.energies[0] || energy > self.energies[self.energies.len() - 1] {
+            return 0.0;
+        }
+
+        let pos = (energy - self.energies[0]) / step;
+        let i = pos.floor() as usize;
+        let w = pos - pos.floor();
+        self.probabilities[i] * (1.0 - w) + self.probabilities[i + 1] * w
+    }
+}
+
+impl Material for SL2 {
+    // Выражение для энергетического спектра (в декартовых координатах)
+    fn energy(&self, p: &Point) -> f64 {
+        EPS1 * (1.0 - p.x.cos() * p.y.cos()) / 2.0
+    }
+
+
+    // Градиент энергии в импульсном пространстве
+    fn energy_gradient(&self, p: &Point) -> Vec2 {
+        Vec2::new(p.x.sin() * p.y.cos(), p.x.cos() * p.y.sin()) * EPS1 / 2.0
+    }
+
+    // Скорость
+    fn velocity(&self, p: &Point) -> Vec2 {
+        self.energy_gradient(&p) * (D / HBAR / C)
+    }
+
+    fn momentums(&self, e: f64, theta: f64) -> Vec<Point> {
+        let samples = 20;
+        let precision = 1e-7;
+        let dir = Vec2::from_polar(1.0, theta);
+        let step = dir * self.brillouin_zone().pmax(theta) / (samples as f64);
+
+        let mut ps: Vec<Point> = Vec::new();
+
+        for i in 0..samples {
+            let mut left = Point::from_vec2(step * i as f64);
+            let mut right = left + step;
+            if (self.energy(&left) - e) * (self.energy(&right) - e) < 0.0 {
+                while (right - left).len() > precision {
+                    let middle = left + (right - left) / 2.0;
+                    if (self.energy(&left) - e) * (self.energy(&middle) - e) < 0.0 {
+                        right = middle;
+                    } else {
+                        left = middle;
+                    }
+                }
+
+                ps.push(left + (right - left) / 2.0);
+            }
+        }
+        ps
+    }
+
+    fn min_energy(&self) -> f64 {
+        self.minimum_energy
+    }
+    fn max_energy(&self) -> f64 {
+        self.maximum_energy
+    }
+    fn brillouin_zone(&self) -> &BrillouinZone {
+        &(self.brillouin_zone)
+    }
+    fn optical_energy(&self) -> f64 {
+        self.optical_energy
+    }
+    fn optical_scattering(&self, p: &Point) -> f64 {
+        self.optical_constant * self.probability(self.energy(p) - self.optical_energy)
+    }
+    fn acoustic_scattering(&self, p: &Point) -> f64 {
+        self.acoustic_constant * self.probability(self.energy(p))
+    }
+}
